ce2de5e2f391ab534f577d60fd943ed3da562dd5
Agaloth <33335971+Agaloth@users.noreply.github.com>
2023-02-02 19:55:17 +0100

    Update TownyWildTownEventListener.java

--- src/main/java/com/agaloth/townywild/listener/TownyWildTownEventListener.java
+++ src/main/java/com/agaloth/townywild/listener/TownyWildTownEventListener.java
@@ -1,19 +1,12 @@
 package com.agaloth.townywild.listeners;
 
+import com.agaloth.townywild.TownyWild;
 import com.agaloth.townywild.tasks.RemoveProtectedPlayerTask;
-import com.agaloth.townywild.tasks.UpdateBossBarPlaceholder;
+import com.agaloth.townywild.utils.Messaging;
+import com.palmergames.bukkit.towny.event.damage.TownyPlayerDamagePlayerEvent;
 import com.palmergames.bukkit.towny.event.player.PlayerEntersIntoTownBorderEvent;
 import com.palmergames.bukkit.towny.event.player.PlayerExitsFromTownBorderEvent;
-import com.palmergames.bukkit.towny.event.damage.TownyPlayerDamagePlayerEvent;
-
-import com.agaloth.townywild.TownyWild;
-
-import me.clip.placeholderapi.PlaceholderAPI;
-import org.bukkit.Bukkit;
-import org.bukkit.boss.BossBar;
-import org.bukkit.boss.BarColor;
-import org.bukkit.boss.BarStyle;
-import org.bukkit.ChatColor;
+import com.palmergames.bukkit.towny.object.Translatable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -23,84 +16,56 @@ import java.util.*;
 import static com.agaloth.townywild.settings.Settings.getConfig;
 
 public class TownyWildTownEventListener implements Listener {
-    @SuppressWarnings("unused")
-    private final TownyWild plugin;
-    private Set<UUID> protectedPlayers = new HashSet<>();
-    public static Map<UUID, Long> scheduledRemovalTimes = new HashMap<>();
-    public static Map<UUID, BossBar> bossBars = new HashMap<>();
-    private Map<UUID, Integer> bossBarTasks = new HashMap<>();
+    public static Set<UUID> protectedPlayers = new HashSet<>();
+    public static Map<UUID, Integer> protectionTimeLeft = new HashMap<>();
 
     public TownyWildTownEventListener(TownyWild instance) {
 
-        plugin = instance;
     }
 
     @EventHandler
-    public void on(TownyPlayerDamagePlayerEvent event) {
+    public void PlayerDamagePlayer(TownyPlayerDamagePlayerEvent event) {
+        // Gets the player being attacked.
         Player victim = event.getVictimPlayer();
+
+        // Gets the attacking player.
         Player attacker = event.getAttackingPlayer();
+
+        // If the protectedPlayers list contains the victim's UUID, it will cancel damages and send a message to the attacker.
         if (protectedPlayers.contains(victim.getUniqueId())) {
             event.setCancelled(true);
-            attacker.sendMessage(ChatColor.YELLOW + "You cannot attack an innocent person while he's on cooldown!");
+            Messaging.sendMsg(attacker, Translatable.of("attacking_player_message"));
             return;
         }
+        // If the protectedPlayers list contains the attacker's UUID, it will cancel damages and send a message to the player being attacked.
         if (protectedPlayers.contains(attacker.getUniqueId())) {
             event.setCancelled(true);
-            attacker.sendMessage(ChatColor.YELLOW + "You cannot attack an enemy while on cooldown!");
+            Messaging.sendMsg(attacker, Translatable.of("victim_player_message"));
         }
     }
 
     @EventHandler
-    public void on(PlayerExitsFromTownBorderEvent event) {
-        System.out.println("Player just exited the town border");
+    public void ExitTownBorder(PlayerExitsFromTownBorderEvent event) {
+        // Gets a player's UUID.
         UUID uuid = event.getPlayer().getUniqueId();
+        System.out.println("Player just exited the town border");
+
+        // Adds a player's UUID to the protectedPlayers list when exiting a town.
         protectedPlayers.add(event.getPlayer().getUniqueId());
+        RemoveProtectedPlayerTask protectionTimeLeft = new RemoveProtectedPlayerTask(uuid);
+        protectionTimeLeft.protectionTimeLeft();
+        System.out.println(protectionTimeLeft + " seconds left of protection");
         System.out.println("protectedPlayers after adding player: " + protectedPlayers);
-        System.out.println("Player is now protected");
-        long scheduledRemovalTime = (Integer.parseInt(Objects.requireNonNull(getConfig().getString("protection_time_after_exiting_town_border"))));
-        System.out.println(scheduledRemovalTime + " seconds left");
-        scheduledRemovalTimes.put(uuid, scheduledRemovalTime);
-        String bossBarText = PlaceholderAPI.setPlaceholders(event.getPlayer(), getConfig().getString("bossbar_message", "You are protected from PVP for %townywild_countdown%!"));
-        bossBarText = PlaceholderAPI.setPlaceholders(event.getPlayer(), bossBarText);
-        BarColor bossBarColor = BarColor.valueOf(getConfig().getString("bossbar_color", "YELLOW"));
-        BarStyle bossBarStyle = BarStyle.valueOf(getConfig().getString("bossbar_style", "SOLID"));
-        BossBar bossBar = Bukkit.createBossBar(bossBarText, bossBarColor, bossBarStyle);
-        bossBar.addPlayer(event.getPlayer());
-        bossBars.put(uuid, bossBar);
-        UpdateBossBarPlaceholder updateTask = new UpdateBossBarPlaceholder(uuid, scheduledRemovalTime, bossBar, this);
-        updateTask.startProgressUpdater();
-        Bukkit.getScheduler().runTaskLater(plugin, new RemoveProtectedPlayerTask(uuid, protectedPlayers, scheduledRemovalTimes), Integer.parseInt(Objects.requireNonNull(getConfig().getString("protection_time_after_exiting_town_border"))));
     }
 
     @EventHandler
-    public void on(PlayerEntersIntoTownBorderEvent event) {
+    public void EnterTownBorder(PlayerEntersIntoTownBorderEvent event) {
+        // Gets a player's UUID
         UUID uuid = event.getPlayer().getUniqueId();
         System.out.println("Player just entered the town border");
-        if (protectedPlayers.contains(uuid)) {
-            protectedPlayers.remove(uuid);
-            System.out.println("protectedPlayers after removing player: " + protectedPlayers);
-            System.out.println("player protection ended");
-            Long scheduledRemovalTime = scheduledRemovalTimes.get(uuid);
-            if (scheduledRemovalTime == 0) {
-                return;
-            }
-            scheduledRemovalTimes.remove(uuid);
-            if (bossBars.containsKey(uuid)) {
-                BossBar existingBossBar = bossBars.get(uuid);
-                Player player = Bukkit.getPlayer(uuid);
-                if (player == null) {
-                    return;
-                }
-                existingBossBar.removePlayer(player);
-                bossBars.remove(uuid);
-                // stop the running task if it exists
-                if (bossBarTasks.containsKey(uuid)) {
-                    Bukkit.getScheduler().cancelTask(bossBarTasks.get(uuid));
-                    bossBarTasks.remove(uuid);
-                }
-            }
-        }
-    }
-    public void remove(UUID uuid) {
+
+        // Removes a player's UUID from the protectedPlayers list when entering a town
+        protectedPlayers.remove(event.getPlayer().getUniqueId());
+        System.out.println("protectedPlayers after removing player: " + protectedPlayers);
     }
 }
