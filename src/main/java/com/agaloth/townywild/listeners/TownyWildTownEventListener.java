4c6bbd10cc3847dd633e6ed1ecad5f92a8c4678a
Agaloth <33335971+Agaloth@users.noreply.github.com>
2023-02-05 10:12:03 +0100

    Update TownyWildTownEventListener.java

--- src/main/java/com/agaloth/townywild/listeners/TownyWildTownEventListener.java
+++ src/main/java/com/agaloth/townywild/listeners/TownyWildTownEventListener.java
@@ -8,6 +8,7 @@ import com.agaloth.townywild.tasks.UpdateBossBarProgress;
 import static com.agaloth.townywild.hooks.TownyWildPlaceholderExpansion.protectionExpirationTime;
 import static com.agaloth.townywild.settings.Settings.getConfig;
 import static com.agaloth.townywild.tasks.UpdateBossBarProgress.timeLeftBar;
+import static com.agaloth.townywild.tasks.UpdateBossBarProgress.uuid;
 
 import com.palmergames.bukkit.towny.TownyMessaging;
 import com.palmergames.bukkit.towny.event.damage.TownyPlayerDamagePlayerEvent;
@@ -27,8 +28,8 @@ import java.util.UUID;
 import java.util.Objects;
 
 public class TownyWildTownEventListener implements Listener {
-    public Map<Player, BukkitTask> cancelProtectionTask = new HashMap<>();
-    public Map<Player, BukkitTask> runningBossBars = new HashMap<>();
+    public Map<UUID, BukkitTask> cancelProtectionTask = new HashMap<>();
+    public Map<UUID, BukkitTask> runningBossBars = new HashMap<>();
     public TownyWildTownEventListener(TownyWild instance) {
     }
 
@@ -41,13 +42,13 @@ public class TownyWildTownEventListener implements Listener {
         Player attacker = event.getAttackingPlayer();
 
         // If the protectionExpirationTime list contains the victim's UUID, it will cancel damages and send a message to the attacker.
-        if (protectionExpirationTime.containsKey(victim)) {
+        if (protectionExpirationTime.containsKey(victim.getUniqueId())) {
             event.setCancelled(true);
             TownyMessaging.sendMessage(attacker, Translatable.of("plugin_prefix").append(Translatable.of("victim_player_message")));
             return;
         }
         // If the protectionExpirationTime list contains the attacker's UUID, it will cancel damages and send a message to the player being attacked.
-        if (protectionExpirationTime.containsKey(attacker)) {
+        if (protectionExpirationTime.containsKey(attacker.getUniqueId())) {
             event.setCancelled(true);
             TownyMessaging.sendMessage(attacker, Translatable.of("plugin_prefix").append(Translatable.of("victim_player_message")));
         }
@@ -59,15 +60,15 @@ public class TownyWildTownEventListener implements Listener {
         int remainingTime = (Integer.parseInt(Objects.requireNonNull(getConfig().getString("protection_time_after_exiting_town_border"))));
 
         // Add player to the hashmap storing expiration times.
-        protectionExpirationTime.put(event.getPlayer(), (long) remainingTime*1000L + System.currentTimeMillis());
+        protectionExpirationTime.put(event.getPlayer().getUniqueId(), (long) remainingTime*1000L + System.currentTimeMillis());
 
         // Runs a Bukkit scheduler to remove the player from the cancelProtectionTask hashmap
         BukkitTask task = Bukkit.getScheduler().runTaskLater(plugin, () -> removePlayerIfExpired(event.getPlayer()), remainingTime*20L);
-        cancelProtectionTask.put(event.getPlayer(), task);
+        cancelProtectionTask.put(event.getPlayer().getUniqueId(), task);
 
         // Runs a Bukkit scheduler to update the bossbar progress and adds the player to the runningBossBars hashmap to remove it when entering a town.
         BukkitTask updateProgress = new UpdateBossBarProgress(event.getPlayer(), remainingTime).runTaskTimer(plugin, 0, 20);
-        runningBossBars.put(event.getPlayer(), updateProgress);
+        runningBossBars.put(event.getPlayer().getUniqueId(), updateProgress);
 
 
         // Shows the bossbar to the player
@@ -80,27 +81,27 @@ public class TownyWildTownEventListener implements Listener {
         UUID uuid = event.getPlayer().getUniqueId();
 
         // Gets the Bukkit task and cancels the protection task.
-        if (cancelProtectionTask.containsKey(event.getPlayer())) {
-            cancelProtectionTask.get(event.getPlayer()).cancel();
+        if (cancelProtectionTask.containsKey(event.getPlayer().getUniqueId())) {
+            cancelProtectionTask.get(event.getPlayer().getUniqueId()).cancel();
         }
 
         // Gets the Bukkit task and cancels the bossbar progress task and sets the progress to 1 to prevent the progress bar from starting at random numbers.
-        if (runningBossBars.containsKey(event.getPlayer())) {
-            runningBossBars.get(event.getPlayer()).cancel();
-            runningBossBars.remove(event.getPlayer());
+        if (runningBossBars.containsKey(event.getPlayer().getUniqueId())) {
+            runningBossBars.get(event.getPlayer().getUniqueId()).cancel();
+            runningBossBars.remove(event.getPlayer().getUniqueId());
             timeLeftBar.setProgress(1);
 
         // Removes the bossbar if a player enters a town.
         timeLeftBar.removePlayer(event.getPlayer());
 
         // Removes a player's UUID from the protectionExpirationTime list when entering a town.
-        protectionExpirationTime.remove(event.getPlayer());
+        protectionExpirationTime.remove(event.getPlayer().getUniqueId());
 
         }
     }
     public void removePlayerIfExpired(Player player) {
         // Removes a player from the protectionExpirationTime hashmap.
-        protectionExpirationTime.remove(player);
+        protectionExpirationTime.remove(uuid);
 
         // Removes the bossbar when the countdown hits 0 seconds.
         timeLeftBar.removePlayer(player);
