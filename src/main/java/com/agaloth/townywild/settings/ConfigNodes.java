3b241c7d992f9f051d6ed928f2716d7d54ae05a2
Agaloth <33335971+Agaloth@users.noreply.github.com>
2023-02-06 07:12:12 +0100

    Config overhaul
    Added some spaces and added the new setting to add blacklisted worlds.

--- src/main/java/com/agaloth/townywild/settings/ConfigNodes.java
+++ src/main/java/com/agaloth/townywild/settings/ConfigNodes.java
@@ -2,37 +2,54 @@ package com.agaloth.townywild.settings;
 
 public enum ConfigNodes {
 
-    VERSION_HEADER("version", "", ""),
+    VERSION_HEADER("version", ""),
     VERSION(
             "version.version",
             "",
             "# This is the current version of TownyWild. DO NOT EDIT THIS."),
+
     LANGUAGE("language",
             "en-US.yml",
             "# The language file you wish to use"),
+
     TOWNY_WILD_ENABLED(
             "towny_wild_enabled",
             "true",
             "",
+
             "############################################################",
             "# +------------------------------------------------------+ #",
             "# |                 PROTECTION NEAR TOWN                 | #",
             "# +------------------------------------------------------+ #",
             "############################################################",
             ""),
+    BLACKLISTED_WORLDS(
+            "blacklisted_worlds",
+            "[]",
+            "",
+            "# This is used to blacklist worlds, players won't get protection in these worlds.",
+            "# You can add worlds to the list by adding this:",
+            "# blacklisted_worlds:",
+            "#   - first_world_name",
+            "#   - second_world_name"),
+
     PROTECTION_AFTER_EXITING_TOWN_BORDER(
             "protection_after_exiting_town_border",
             "true",
             "",
             "# If this value is true, players will get protection near town blocks."),
+
     PROTECTION_TIME_AFTER_EXITING_TOWN_BORDER(
             "protection_time_after_exiting_town_border",
             "20",
+            "",
             "# This is the amount of time in seconds, the player exiting the town border will have PvP protection for, during that time, the player cannot hit or get hit by other players."),
+
     BOSSBAR_ENABLED(
             "bossbar_enabled",
             "true",
-            "#",
+            "",
+
             "############################################################",
             "# +------------------------------------------------------+ #",
             "# |                   BOSSBAR SETTINGS                   | #",
@@ -42,14 +59,19 @@ public enum ConfigNodes {
     BOSSBAR_COLOR(
             "bossbar_color",
             "YELLOW",
+            "",
             "# This is the color for the bossbar, you can use BLUE, GREEN, PINK, PURPLE, RED, WHITE and YELLOW"),
+
     BOSSBAR_STYLE(
             "bossbar_style",
             "SOLID",
+            "",
             "# This is the style for the bossbar, you can use SOLID, SEGMENTED_6, SEGMENTED_10, SEGMENTED_12 and SEGMENTED_20"),
+
     BOSS_BAR_MESSAGE(
             "bossbar_message",
             "&e&lYou are protected for %townywild_countdown%!",
+            "",
             "# This is the message that will be displayed on the boss bar. (Use the %townywild_countdown% placeholder if you want to include the amount of time left before their protection ends)");
 
     private final String Root;
