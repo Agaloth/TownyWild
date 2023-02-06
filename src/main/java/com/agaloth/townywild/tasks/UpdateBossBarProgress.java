a38600da19869897ca6fb675ed94d11a5599ae72
Agaloth <33335971+Agaloth@users.noreply.github.com>
2023-02-06 07:18:29 +0100

    Update UpdateBossBarProgress.java

--- src/main/java/com/agaloth/townywild/tasks/UpdateBossBarProgress.java
+++ src/main/java/com/agaloth/townywild/tasks/UpdateBossBarProgress.java
@@ -1,6 +1,5 @@
 package com.agaloth.townywild.tasks;
 
-import com.agaloth.townywild.settings.ConfigNodes;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
