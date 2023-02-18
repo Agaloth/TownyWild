0165954e7f37708dfad85334cbb47150edc6340b
Agaloth <33335971+Agaloth@users.noreply.github.com>
2023-02-18 23:48:44 +0100

    Added more comments

--- src/main/java/com/agaloth/townywild/hooks/TownyWildPlaceholderExpansion.java
+++ src/main/java/com/agaloth/townywild/hooks/TownyWildPlaceholderExpansion.java
@@ -1,123 +1,148 @@
-package com.agaloth.townywild.listeners;
+package com.agaloth.townywild.hooks;
 
-import com.agaloth.townywild.TownyWild;
-import static com.agaloth.townywild.TownyWild.plugin;
-
-import com.agaloth.townywild.tasks.UpdateBossBarProgress;
-
-import static com.agaloth.townywild.hooks.TownyWildPlaceholderExpansion.protectionExpirationTime;
-import static com.agaloth.townywild.settings.Settings.getConfig;
-import static com.agaloth.townywild.tasks.UpdateBossBarProgress.*;
-
-import com.palmergames.bukkit.towny.TownyMessaging;
-import com.palmergames.bukkit.towny.event.damage.TownyPlayerDamagePlayerEvent;
-import com.palmergames.bukkit.towny.event.player.PlayerEntersIntoTownBorderEvent;
-import com.palmergames.bukkit.towny.event.player.PlayerExitsFromTownBorderEvent;
-import com.palmergames.bukkit.towny.object.Translatable;
-
-import org.bukkit.Bukkit;
-import org.bukkit.boss.BossBar;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Map;
 import java.util.HashMap;
-import java.util.UUID;
+import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
-public class TownyWildTownEventListener implements Listener {
-    public Map<UUID, BukkitTask> cancelProtectionTask = new HashMap<>();
-    public Map<UUID, BukkitTask> runningBossBars = new HashMap<>();
+import static com.agaloth.townywild.TownyWild.plugin;
+import static com.agaloth.townywild.settings.Settings.getConfig;
+import static com.agaloth.townywild.tasks.UpdateBossBarProgress.uuid;
 
-    public TownyWildTownEventListener(TownyWild instance) {
+public class TownyWildPlaceholderExpansion extends PlaceholderExpansion {
+    public static Map<UUID, Long> protectionExpirationTime = new HashMap<>();
+
+    public TownyWildPlaceholderExpansion() {
     }
 
-    @EventHandler
-    public void PlayerDamagePlayer(TownyPlayerDamagePlayerEvent event) {
-        // Gets the player being attacked.
-        Player victim = event.getVictimPlayer();
+    public long getRemainingProtectionTime(Player player) {
 
-        // Gets the attacking player.
-        Player attacker = event.getAttackingPlayer();
+        // Takes the current time in milliseconds
+        long now = System.currentTimeMillis();
 
-        // If the protectionExpirationTime list contains the victim's UUID, it will cancel damages and send a message to the attacker.
-        if (protectionExpirationTime.containsKey(victim.getUniqueId())) {
-            event.setCancelled(true);
-            TownyMessaging.sendMessage(attacker, Translatable.of("plugin_prefix").append(Translatable.of("victim_player_message")));
-            return;
+        // Gets the expiration time from the protectionExpirationTime hashmap
+        long expireTime = protectionExpirationTime.get(uuid);
+
+        // Then the placeholder returns a countdown with the following formula:
+        return (expireTime - now) / 1000;
+    }
+
+    public long setRemainingProtectionTime(Player player) {
+
+        // Takes the protection time from the config file.
+        long configTime = (Integer.parseInt(Objects.requireNonNull(getConfig().getString("protection_time_after_exiting_town_border"))));
+
+        // Uses a formula which takes the currentTimeMillis and adds the protection time from the config file multiplied by 1000.
+        long expireTime = System.currentTimeMillis() + (configTime * 1000);
+
+            // Creates a new Bukkit runTaskTimer runnable
+            new BukkitRunnable() {
+
+                @Override
+                public void run() {
+                    // If currentTimeMillis is higher or equal to the expireTime it will remove the player's protection.
+                    if (System.currentTimeMillis() >= expireTime) {
+                        protectionExpirationTime.remove(uuid, expireTime);
+                    }
+
+                }
+            }.runTaskLater(plugin, configTime*20);
+        return expireTime;
+    }
+
+
+    /**
+     * This class will automatically register as a placeholder expansion
+     * when a jar including this class is added to the directory
+     * {@code /plugins/PlaceholderAPI/expansions} on your server.
+     */
+
+
+    /**
+     * This method should always return true unless we
+     * have a dependency we need to make sure is on the server
+     * for our placeholders to work!
+     *
+     * @return always true since we do not have any dependencies.
+     */
+    @Override
+    public boolean canRegister() {
+        return true;
+    }
+
+    /**
+     * The name of the person who created this expansion should go here
+     */
+    @Override
+    public String getAuthor() {
+        return "Agaloth";
+    }
+
+    /**
+     * The placeholder identifier should go here
+     * This is what tells PlaceholderAPI to call our onPlaceholderRequest method to obtain
+     * a value if a placeholder starts with our identifier.
+     * This must be unique and can not contain % or _
+     */
+    @Override
+    public @NotNull String getIdentifier() {
+        return "townywild";
+    }
+
+    /**
+     * if an expansion requires another plugin as a dependency, the proper name of the dependency should
+     * go here. Set this to null if your placeholders do not require another plugin be installed on the server
+     * for them to work. This is extremely important to set if you do have a dependency because
+     * if your dependency is not loaded when this hook is registered, it will be added to a cache to be
+     * registered when plugin: "getPlugin()" is enabled on the server.
+     */
+    @Override
+    public @NotNull String getPlugin() {
+        return "TownyWild";
+    }
+
+    /**
+     * This is the version of this expansion
+     */
+    @Override
+    public @NotNull String getVersion() {
+        return "1.0.0";
+    }
+
+    /**
+     * This is the method called when a placeholder with our identifier is found and needs a value
+     * We specify the value identifier in this method
+     */
+    @Override
+    public String onRequest(OfflinePlayer player, String identifier) {
+
+        // If the placeholder is %townywild_countdown%
+        if (identifier.equals("countdown")) {
+
+            long remainingProtectionTimeInSeconds = getRemainingProtectionTime((Player) player);
+
+            // If the remaining protection time is greater than 60 seconds.
+            if (remainingProtectionTimeInSeconds > 60) {
+
+                // Convert it to minutes.
+                long remainingProtectionTimeInMinutes = TimeUnit.SECONDS.toMinutes(remainingProtectionTimeInSeconds);
+
+                // Return minutes instead of seconds (example: 1 minute).
+                return Long.toString(remainingProtectionTimeInMinutes) + " minutes";
+
+            } else {
+
+                // Return seconds.
+                return Long.toString(remainingProtectionTimeInSeconds) + " seconds";
+            }
+            }
+            return "";
         }
-
-        // If the protectionExpirationTime list contains the attacker's UUID, it will cancel damages and send a message to the player being attacked.
-        if (protectionExpirationTime.containsKey(attacker.getUniqueId())) {
-            event.setCancelled(true);
-            TownyMessaging.sendMessage(attacker, Translatable.of("plugin_prefix").append(Translatable.of("victim_player_message")));
-        }
-    }
-
-    @EventHandler
-    public void ExitTownBorder(PlayerExitsFromTownBorderEvent event) {
-        // Gets the remaining time from the config file.
-        int remainingTime = (Integer.parseInt(Objects.requireNonNull(getConfig().getString("protection_time_after_exiting_town_border"))));
-
-        // Add player to the hashmap storing expiration times.
-        protectionExpirationTime.put(event.getPlayer().getUniqueId(), (long) remainingTime*1000L + System.currentTimeMillis());
-
-        // Runs a Bukkit scheduler to remove the player from the cancelProtectionTask hashmap.
-        BukkitTask task = Bukkit.getScheduler().runTaskLater(plugin, () -> removePlayerIfExpired(event.getPlayer()), remainingTime*20L);
-        cancelProtectionTask.put(event.getPlayer().getUniqueId(), task);
-
-        // Runs a Bukkit scheduler to update the bossbar progress and adds the player to the runningBossBars hashmap to remove it when entering a town.
-        BukkitTask updateProgress = new UpdateBossBarProgress(event.getPlayer(), remainingTime).runTaskTimer(plugin, 0, 20);
-        runningBossBars.put(event.getPlayer().getUniqueId(), updateProgress);
-
-        // Shows the bossbar to the player.
-        BossBar timeLeftBar = createBossBar.get(event.getPlayer().getUniqueId());
-
-        // Adds the player to the timeLeftBar bossbar.
-        timeLeftBar.addPlayer(event.getPlayer());
-    }
-
-    @EventHandler
-    public void EnterTownBorder(PlayerEntersIntoTownBorderEvent event) {
-        // Gets a player's UUID
-        UUID uuid = event.getPlayer().getUniqueId();
-
-        // Gets the Bukkit task and cancels the protection task.
-        if (cancelProtectionTask.containsKey(event.getPlayer().getUniqueId())) {
-            cancelProtectionTask.get(event.getPlayer().getUniqueId()).cancel();
-        }
-
-        // Gets the Bukkit task and cancels the bossbar progress task and sets the progress to 1 to prevent the progress bar from starting at random numbers.
-        if (runningBossBars.containsKey(event.getPlayer().getUniqueId())) {
-            runningBossBars.get(event.getPlayer().getUniqueId()).cancel();
-            runningBossBars.remove(event.getPlayer().getUniqueId());
-
-            // Gets the player's uuid from the createBossBar hashmap.
-            createBossBar.get(event.getPlayer().getUniqueId());
-
-            // Removes the bossbar if a player enters a town.
-            BossBar timeLeftBar = createBossBar.remove(event.getPlayer().getUniqueId());
-            timeLeftBar.removePlayer(event.getPlayer());
-
-            // Removes a player's UUID from the protectionExpirationTime list when entering a town.
-            protectionExpirationTime.remove(event.getPlayer().getUniqueId());
-
-        }
-    }
-    public void removePlayerIfExpired(Player player) {
-        // Removes a player from the protectionExpirationTime hashmap.
-        protectionExpirationTime.remove(player.getUniqueId());
-
-        // Removes the bossbar when the countdown hits 0 seconds.
-        createBossBar.get(player.getUniqueId());
-
-        // Removes the bossbar if a player enters a town.
-        BossBar timeLeftBar = createBossBar.remove(player.getUniqueId());
-        timeLeftBar.removePlayer(Objects.requireNonNull(player.getPlayer()));
-
-        // Sends a message to the protected player telling them that their protection has ended.
-        TownyMessaging.sendMessage(player, Translatable.of("plugin_prefix").append(Translatable.of("player_protection_ended")));
-    }
 }
